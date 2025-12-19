44e38513f0beef49baa62d89147aff79016e40d7
Fabian Gonzalez <fabian.gonzalez@solo.io>
2025-12-19 10:47:58 -0500

    add todo
    Signed-off-by: Fabian Gonzalez <fabian.gonzalez@solo.io>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -5,6 +5,7 @@ import (
 )
 
 // Exposing internal client for external use
+// TODO: It _may_ be worth creating a public client or client interface so external libraries using this can extend it
 func NewClientFromEnv() (*client.Client, error) {
 	return client.NewClientFromEnv()
 }
